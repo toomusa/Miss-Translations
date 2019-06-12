d5b7b23077c314706f7bffbb5c58fa096fd14a86
toomusa <3895499+toomusa@users.noreply.github.com>
2019-06-11 19:28:12 -0700

    cleaned up the code

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,12 +1,9 @@
 
 let word = "sprint";
-// let userArray = [];
 let userObject = {};
 let renderedObject = {};
 var userArray = "";
 var userInput = "";
-// var anto = "";
-// var partOfSpeech = "";
 let newUserObject = {};
 let newUserArray = [];
 var sentence = "";
@@ -16,7 +13,6 @@ const randomOf = (arr) => {
     return chosenArr;
 }
 
-
 $(document).on("click", "#submit-request", function(){
     userInput = $("#user-input").val().trim();
     userArray = userInput.split(" ");
@@ -25,25 +21,14 @@ $(document).on("click", "#submit-request", function(){
     userObject = {};
     sentence = "";
     replaceWords();
+    console.log(userArray);
 })
-
-// take in user input and turn it into array
-
-// userInput = "I ate an apple";
-
-
-// userArray = ["I", "ate", "an", "apple"];
-
-console.log(userArray);
-
-// take in user input array and turn it into an object
 
 function toObject(userArray) {
     for (let i = 0; i < userArray.length; ++i)
         userObject[i] = userArray[i];
     return userObject;
   }
-
 
 const replaceWords = async () => {
     toObject(userArray);
@@ -55,17 +40,6 @@ const replaceWords = async () => {
     console.log("[getWord] ", renderedObject); 
     replacePhrase();
 }
-
-// const speechPart = () => {
-//     for (let key in userObject) {
-//         if (partOfSpeech === "noun") {
-//             userObject[key] = anto;
-//         }
-//         if (partOfSpeech === "verb") {
-//             userObject[key] = anto;
-//         }
-//     }
-// }
 
 const getWordPromise = (word) => {
     return new Promise ((resolve, reject) => {
@@ -99,57 +73,12 @@ const getWordPromise = (word) => {
                     }
                 }
                 console.log(renderedObject);
-                // replacePhrase();
             }
             resolve()
         })
     })
 }
 
-
-// const getWord = (word) => {
-//     var queryURL = `https://wordsapiv1.p.mashape.com/words/${word}`
-//     $.ajax ({
-//         url: queryURL,
-//         method: "GET",
-//         headers: {
-//             "X-Mashape-Key": "80c3fabd83mshc44d9723dc45a9ap1da501jsn8faca156e252"
-//         }
-//     }).then(function(response){
-//         console.log(response);
-//         var anto = "";
-//         var partOfSpeech = "";
-//         if (response.results){
-//             for (let i = 0; i < response.results.length; i++) {
-//                 if (response.results[i].antonyms) {
-//                     anto = response.results[i].antonyms.trim();
-//                     partOfSpeech = response.results[i].partOfSpeech.trim();
-//                     if (partOfSpeech === "noun" || partOfSpeech === "verb") {
-//                         renderedObject[word] = anto;
-//                     }
-//                 } else if (response.results[i].synonyms) {
-//                     anto = response.results[i].synonyms[i].trim();
-//                     partOfSpeech = response.results[i].partOfSpeech.trim();
-//                     if (partOfSpeech === "noun" || partOfSpeech === "verb") {
-//                         renderedObject[word] = anto;
-//                     }
-//                 } else {
-//                 }
-//             }
-//             console.log(renderedObject);
-//             // replacePhrase();
-//         }
-//         console.log("antonym: " + anto);
-//         console.log("part of speech: " + partOfSpeech);
-//     })
-// }
-
-/*
-renderedObject
-{an: "associate in nursing", apple: "malus pumila", I: "one"}
-userObject
-{0: "I", 1: "ate", 2: "an", 3: "apple"}
-*/
 const replacePhrase = () => {
     for (let k = 0; k < Object.values(userObject).length; k++) {
         if (renderedObject[userObject[k]]) {
@@ -164,8 +93,6 @@ const replacePhrase = () => {
     console.log("replacePhrase", sentence)
     getTranslate(sentence);
 }
-// newUserInput = newUserArray.toString();
-// getTranslate(newUserInput);
 
 const getTranslate = (sentence) => {
     qURL = `https://translation.googleapis.com/language/translate/v2?target=es&q=${sentence}&key=AIzaSyClTfWgaANNnxJloF2kStJQwZorF8JhaG4`;
